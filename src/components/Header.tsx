76933b58ac89dd45c37a163dd206625e6cc6cf4f
euijin <aghpt04@gmail.com>
2024-09-01 15:01:14 +0900

    🐛 :: 헤더 css 오류 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,16 @@ import SearchIcon from "../assets/SearchIcon.svg";
 import PersonIcon from "../assets/PersonIcon.svg";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
     const [searchBox, setSearchBox] = useState<boolean>(false);
     return (
         <>
             <Wrapper>
-                <img src={Logo}></img>
+                <Link to={"/"}>
+                    <img src={Logo}></img>
+                </Link>
                 <div>
                     <SearchWrap>
                         <input
@@ -28,7 +31,9 @@ export const Header = () => {
                             }}
                         />
                     </SearchWrap>
-                    <img className="icon" src={PersonIcon} />
+                    <Link to={"/profile"} style={{ display: "flex", justifyContent: "center" }}>
+                        <img className="icon" src={PersonIcon} />
+                    </Link>
                 </div>
             </Wrapper>
             <Outlet />
@@ -42,23 +47,21 @@ const Wrapper = styled.div`
     align-items: center;
     position: fixed;
     background-color: white;
+    justify-content: space-between;
+    padding: 0 350px;
+    box-sizing: border-box;
+    z-index: 1000;
     height: 65px;
     border-bottom: 1px solid rgba(0, 0, 0, 0.16);
+    @media (max-width: 1200px) {
+        padding: 0 70px;
+    }
     > div {
         display: flex;
         gap: 36px;
-        padding-right: 280px;
-        @media (max-width: 1200px) {
-            padding-right: 100px;
-        }
     }
     > img {
         cursor: pointer;
-        margin-right: auto;
-        padding-left: 280px;
-        @media (max-width: 1200px) {
-            padding-left: 100px;
-        }
     }
     .icon {
         width: 24px;
